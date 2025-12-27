06c5347d90b54080e761e632f3418282a385031b
GabrielAguiar0704 <gabriel.aguiar.studio@gmail.com>
2025-12-27 00:56:14 +0000

    feat: dotenv configured

---
+++ SQL SCRIPTS/create_users.sql
@@ -0,0 +1,9 @@
+USE nankinpress_db;
+
+CREATE TABLE users (
+	user_id int,
+    user_name varchar(31),
+    user_username varchar(15),
+    user_password varchar(31),
+    user_mail varchar(31)
+);
--- server.js
+++ server.js
@@ -1,14 +1,41 @@
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 5000;
+dotenv.config();
+const HOST = process.env.HOST;
+const USER = process.env.USER;
+const PASSWORD = process.env.PASSWORD;
+const DATABASE = process.env.DATABASE;
 
 app.use(express.static(path.join(__dirname, "public")));
+
+try {
+    var pool = await mysql.createPool({
+    host: HOST,
+    user: USER,
+    password: PASSWORD,
+    database: DATABASE
+    }).promise();
+    console.log("Sucesso ao criar a pool", pool);
+} catch(error) {
+    console.log(error);
+}
+
+try {
+    const result = await pool.query('SELECT 1');
+    console.log("SELECT funcionando");
+}
+catch (error){
+    console.log(error);
+}
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/index.html")
